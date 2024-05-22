0831a6b28d379fc8538437c62282ded140074ea5
Parth Dhiman <cynical.melomaniac@gmail.com>
2024-05-22 18:03:05 +0530

    second draft on text_renderer and skeleton of editor logic with classes

--- src/editor.hpp
+++ src/editor.hpp
@@ -3,12 +3,27 @@
 #include "app.hpp"
 
 namespace particle {
-class io_handler{
+class io_handler {
   private:
   std::fstream stream;
   public:
   io_handler(std::string file_path);
   ~io_handler();
   std::string return_stream_data();
+};
+class buffer_handler {
+  private:
+  std::string buffer;
+  public:
+  buffer_handler();
+  ~buffer_handler();
+  void flush_and_fill_buffer(io_handler io);
+};
+class event_handler {
+  private:
+  event_handler();
+  ~event_handler();
+  bool export_event_bool();
+  std::string export_event_str();
 };
 }
